536d48d0d291f25abaeaa93f61a0a02201d0ad92
wjdgus1122 <qodqodantk@gmail.com>
2022-06-27 09:52:20 +0900

    get movies

--- README.md
+++ README.md
@@ -6,10 +6,20 @@
 - [x] router setup
 - [x] Create Components
 - [x] Helmet
-- [ ] Get Api
-- [ ] Use Effect
+- [x] Get Api
+
+## Main page
+
+- [x] Use Effect
 - [ ] try catch
 - [ ] Header Event
 - [ ] Slide
+
+## Detail page
+
 - [ ] Detail Page
 - [ ] Video
+
+## Search page
+
+- [ ] Css Grid
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,17 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { movieApi } from "../../../api";
 
 export const Home = () => {
+  const [playing, setPlaying] = useState();
+  const [rated, setRated] = useState();
+  const [upcome, setUpcome] = useState();
+
   useEffect(() => {
     const movieData = async () => {
-      // console.log(await movieApi.nowPlaying());
-      const {
-        data: { results },
-      } = await movieApi.nowPlaying();
-      console.log(results);
-      // console.log(playing);
+      try {
+        const {
+          data: { results: playingData },
+        } = await movieApi.nowPlaying();
+        setPlaying(playingData);
+
+        const {
+          data: { results: ratedData },
+        } = await movieApi.topRated();
+        // =>비구조화 할당 이용시  변수명 변경할때는
+        // 변수명:변경할이름
+        setRated(ratedData);
+
+        const {
+          data: { results: upcomeData },
+        } = await movieApi.upComing();
+        setUpcome(upcomeData);
+      } catch (error) {
+        console.log(error);
+      }
+      // =>예외처리 해주는 if/else문 같은 느낌
+      // console.log(movieApi.latest())
     };
     movieData();
   }, []);
-  return <>Home!</>;
+  // console.log(`현재상영 영화: ${playing}`);
+  // console.log(`인기 영화: ${rated}`);
+  // console.log(`최신 영화: ${late}`);
+  console.log("상영예정 영화:", upcome);
+  return <></>;
 };
